6b446dd864319f154479a591f25167ea4d647305
Aljaz200 <as67643@student.uni-lj.si>
2023-11-17 03:09:59 +0100

    DN06

--- Domače naloge/DN06 - Autocomplete/Autocomplete.cpp
+++ Domače naloge/DN06 - Autocomplete/Autocomplete.cpp
@@ -4,161 +4,50 @@
 #include <string>
 using namespace std;
 
-// za dodajanje uporabimo binary search, enako iskanje.
+// najprej sortiramo, nato pa binary search
 
-class Slovar {
+class Slovar{
 private:
-    vector<int> t;
-    vector<string> besede;
-    vector<int> pozicije;
+    vector<pair<string, int>> t;
+    int N;
 public:
-    void push(string s, int x, int poz) {
+    Slovar(int n){
+        N = n;
+    }
+    void dodaj(int poz, string beseda){
+        t.push_back(make_pair(beseda, poz));
+    }
+
+    void sortiraj(){
+        sort(t.begin(), t.end());
+    }
+
+    void izpisi(vector<int> stevila){
+        for (int i = 0; i < N; i++){
+            cout << t[i].first << " " << stevila[t[i].second - 1] << " " << t[i].second << endl;
+        }
+    }
+
+    pair<string, int> operator[](int i){
+        return t[i];
+    }
+
+    int poisci(string s, vector<int> stevila){
         int min = 0;
         int dolzina1 = (int)s.length();
         int dolzina2, dolzina;
-        int pozic = 0;
-
-        if (t.size() == 0){
-            t.push_back(x);
-            besede.push_back(s);
-            pozicije.push_back(poz);
-            return;
-        }
-        if (primerjaj(s, t.size()-1, dolzina1) == 1){
-            t.push_back(x);
-            besede.push_back(s);
-            pozicije.push_back(poz);
-            pozic = t.size() - 1;
-        }else{
-            if (primerjaj(s, 0, dolzina1) == 1){
-                int mid;
-                int max = t.size() - 1;
-                while (min < max){
-                    mid = (min + max) / 2;
-                    int rez = primerjaj(s, mid, dolzina1);
-                    if (rez == -1){
-                        max = mid;
-                    }
-                    else if (rez == 1){
-                        min = mid + 1;
-                    }else{
-                        if (t[mid] < x){
-                            t[mid]= x;
-                            pozicije[mid]= poz;
-                        }
-                        return;
-                    }
-                }
-                
-            }
-            
-            pozic = min;
-            
-            int velikost = (int)t.size();
-            while (min < velikost){
-                dolzina2 = (int)besede[min].length();
-                dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
-                int rez = 0;
-                for (int i = 0; i < dolzina; i++){                    
-                    if (s[i] < besede[min][i]){
-                        rez = -1;
-                        
-                        t.insert(t.begin() + pozic, x);
-                        besede.insert(besede.begin() + pozic, s);
-                        pozicije.insert(pozicije.begin() + pozic, poz);
-                        break;
-                    }
-                }
-                if (rez != -1){
-                    if (t[min] >= x){
-                        return;
-                    }
-                    if (min == velikost - 1){
-                        t.insert(t.begin() + pozic, x);
-                        besede.insert(besede.begin() + pozic, s);
-                        pozicije.insert(pozicije.begin() + pozic, poz);
-                    }
-                }else{
-                    break;
-                }
-                min++;
-            }
-        }
-        
-        min = pozic;
-        while (min > 0){
-            min--;
-            dolzina2 = (int)besede[min].length();
-            dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
-            if (s[0] > besede[min][0]){
-                return;
-            }
-            int pr = 0;
-            for (int i = 0; i < dolzina; i++){
-                if (s[i] != besede[min][i]){
-                    pr = 1;
-                    break;;
-                }
-            }
-            if (pr == 1){
-                continue;
-            }
-            if (t[min] < x){
-                t.erase(t.begin() + min);
-                besede.erase(besede.begin() + min);
-                pozicije.erase(pozicije.begin() + min);
-            }else{
-                return;
-            }
-        }
-        /*
-        t.insert(t.begin() + pozic, temp);
-        besede.insert(besede.begin() + pozic, s);
-        */
-        return;
-    }
-
-    int primerjaj (string s, int poz, int dolzina1) {
-        int dolzina2 = (int)besede[poz].length();
-        int dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
-        for (int i = 0; i < dolzina; i++){
-            if (s[i] < besede[poz][i]){
-                return -1;
-            }
-            else if (s[i] > besede[poz][i]){
-                return 1;
-            }
-        }
-        if (dolzina1 < dolzina2){
-            return -1;
-        }else if (dolzina1 == dolzina2){
+        if (t[N-1].first.compare(s) < 0){
             return 0;
-        }
-        return 1;
-    }
-
-    void izpisi(){
-        for (int i = 0; i < (int)t.size(); i++){
-            cout << besede[i] << " " << t[i] << " " << pozicije[i] << endl;
-        }
-    }
-
-    int poisci(string s){
-        int min = 0;
-        int dolzina1 = (int)s.length();
-        int dolzina2, dolzina;
-        if (primerjaj(s, t.size()-1, dolzina1) == 1){
-            return 0;
-        }else if(primerjaj(s, 0, dolzina1) != -1){
+        }else if(t[0].first.compare(s) <= 0){
             int mid;
-            int max = t.size() - 1;
+            int max = N - 1;
             while (min < max){
                 mid = (min + max) / 2;
-                int rez = primerjaj(s, mid, dolzina1);
-                if (rez == -1){
+                int rez = t[mid].first.compare(s);
+                if (rez > 0){
                     max = mid;
                 }
-                else if (rez == 1){
+                else if (rez < 0){
                     min = mid + 1;
                 }else{
                     min = mid;
@@ -168,50 +57,50 @@ public:
         }
         int max = 0;
         int poz = 0;
-        while (min < (int)t.size()){
-            dolzina2 = (int)besede[min].length();
+        while (min < N){
+            dolzina2 = (int)t[min].first.length();
             dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
-            int rez = 0;
             for (int i = 0; i < dolzina; i++){
-                if (s[i] < besede[min][i]){
-                    rez = -1;
-                    break;
+                if (s[i] != t[min].first[i]){
+                    return poz;
                 }
             }
-            if (rez != -1){
-                if (t[min] > max){
-                    max = t[min];
-                    poz = pozicije[min];
-                }
-            }else{
-                break;
+            if (stevila[t[min].second - 1] > max){
+                max = stevila[t[min].second - 1];
+                poz = t[min].second;
             }
             min++;
         }
         return poz;            
     }
-
 };
 
+
+
 int main(){
-    int N, x;
+    int N, x, M;
     string s;
     cin >> N;
-    Slovar v;
-    for (int i = 1; i <= N; i++){
+    Slovar v(N);
+    vector<int> stevila;
+    for (int i = 1; i <=N; i++){
         cin >> s >> x;
-        v.push(s, x, i);
+        v.dodaj(i, s);
+        stevila.push_back(x);
+    }
+    v.sortiraj();
+    //v.izpisi(stevila);
+
+    cin >> M;
+    int *rezultati = new int[M];
+    for (int i = 0; i < M; i++){
+        cin >> s;
+        rezultati[i] = v.poisci(s, stevila);
+    }
+
+    for (int i = 0; i < M; i++){
+        cout << rezultati[i] << endl;
     }
     
-    cin >> N;
-    int *izpis = new int[N];
-    for (int i = 0; i < N; i++){
-        cin >> s;
-        izpis[i] = v.poisci(s);
-    }
-    //v.izpisi();
-    for (int i = 0; i < N; i++){
-        cout << izpis[i] << endl;
-    }
     return 0;
 }
--- Domače naloge/DN06 - Autocomplete/Autocomplete1.cpp
+++ Domače naloge/DN06 - Autocomplete/Autocomplete1.cpp
@@ -4,330 +4,144 @@
 #include <string>
 using namespace std;
 
-// za dodajanje uporabimo binary search, enako iskanje.
+// najprej sortiramo, nato pa binary search
 
-int main(){
-    int N, x;
-    string s;
-    cin >> N;
-    //Slovar v;
-    vector<int> t;
-    vector<string> besede;
-    vector<int> pozicije;
-    cin >> s >> x;
-    t.push_back(x);
-    besede.push_back(s);
-    pozicije.push_back(1);
-    for (int i = 2; i <= N; i++){
-        cin >> s >> x;
-        int poz = i;
+class Slovar{
+private:
+    vector<pair<string, int>> t;
+    int N;
+public:
+    Slovar(int n){
+        N = n;
+    }
+    void dodaj(int poz, string beseda){
+        t.push_back(make_pair(beseda, poz));
+    }
+
+    void sortiraj(){
+        sort(t.begin(), t.end());
+    }
+
+    void izpisi(vector<int> stevila){
+        for (int i = 0; i < N; i++){
+            cout << t[i].first << " " << stevila[t[i].second - 1] << " " << t[i].second << endl;
+        }
+    }
+
+    pair<string, int> operator[](int i){
+        return t[i];
+    }
+
+    int poisci(string s, vector<int> stevila){
         int min = 0;
         int dolzina1 = (int)s.length();
         int dolzina2, dolzina;
-        int pozic = 0;
-        int bb = 0;
-        int temp;
-
-        int rez = 0;
-        temp = t.size() - 1;
-        dolzina2 = (int)besede[temp].length();
-        dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
-        for (int i = 0; i < dolzina; i++){
-            if (s[i] < besede[temp][i]){
-                rez = -1;
-                break;
-            }
-            else if (s[i] > besede[temp][i]){
-                rez = 1;
-                break;
+        if (t[N-1].first.compare(s) < 0){
+            return 0;
+        }else if(t[0].first.compare(s) <= 0){
+            int mid;
+            int max = N - 1;
+            while (min < max){
+                mid = (min + max) / 2;
+                int rez = t[mid].first.compare(s);
+                if (rez > 0){
+                    max = mid;
+                }
+                else if (rez < 0){
+                    min = mid + 1;
+                }else{
+                    min = mid;
+                    break;
+                }
             }
         }
-        if (rez == 0){
-            if (dolzina1 < dolzina2){
-                rez = -1;
-            }else if (dolzina1 == dolzina2){
-                rez = 0;
-            }else{
-                rez = 1;
+        dolzina2 = (int)t[min].first.length();
+        dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
+        for (int i = 0; i < dolzina; i++){
+            if (s[i] != t[min].first[i]){
+                return 0;
             }
         }
         
-        if (rez == 1){
-            t.push_back(x);
-            besede.push_back(s);
-            pozicije.push_back(poz);
-            pozic = t.size() - 1;
-        }else{
-            rez = 0;
-            temp = 0;
-            dolzina2 = (int)besede[temp].length();
-            dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
-            for (int i = 0; i < dolzina; i++){
-                if (s[i] < besede[temp][i]){
-                    rez = -1;
-                    break;
-                }
-                else if (s[i] > besede[temp][i]){
-                    rez = 1;
-                    break;
-                }
-            }
-            if (rez == 0){
-                if (dolzina1 < dolzina2){
-                    rez = -1;
-                }else if (dolzina1 == dolzina2){
-                    rez = 0;
-                }else{
-                    rez = 1;
-                }
-            }
-            if (rez == 1){
-                int mid;
-                int max = t.size() - 1;
-                while (min < max){
-                    mid = (min + max) / 2;
-                    rez = 0;
-                    temp = mid;
-                    dolzina2 = (int)besede[temp].length();
-                    dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
-                    for (int i = 0; i < dolzina; i++){
-                        if (s[i] < besede[temp][i]){
-                            rez = -1;
-                            break;
-                        }
-                        else if (s[i] > besede[temp][i]){
-                            rez = 1;
-                            break;
-                        }
-                    }
-                    if (rez == 0){
-                        if (dolzina1 < dolzina2){
-                            rez = -1;
-                        }else if (dolzina1 == dolzina2){
-                            rez = 0;
-                        }else{
-                            rez = 1;
-                        }
-                    }
-
-                    if (rez == -1){
-                        max = mid;
-                    }
-                    else if (rez == 1){
-                        min = mid + 1;
-                    }else{
-                        if (t[mid] < x){
-                            t[mid]= x;
-                            pozicije[mid]= poz;
-                        }
-                        bb = 1;
-                        break;
-                    }
-                }
-                
-            }
-            if (bb == 1){
-                continue;
-            }
-            
-            pozic = min;
-            
-            int velikost = (int)t.size();
-            while (min < velikost){
-                dolzina2 = (int)besede[min].length();
-                dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
-                int rez = 0;
-                for (int i = 0; i < dolzina; i++){                    
-                    if (s[i] < besede[min][i]){
-                        rez = -1;
-                        
-                        t.insert(t.begin() + pozic, x);
-                        besede.insert(besede.begin() + pozic, s);
-                        pozicije.insert(pozicije.begin() + pozic, poz);
-                        break;
-                    }
-                }
-                if (rez != -1){
-                    if (t[min] >= x){
-                        break;
-                    }
-                    if (min == velikost - 1){
-                        t.insert(t.begin() + pozic, x);
-                        besede.insert(besede.begin() + pozic, s);
-                        pozicije.insert(pozicije.begin() + pozic, poz);
-                    }
-                }else{
-                    bb = 1;
-                    break;
-                }
-                min++;
-            }
-        }
-        if (bb == 1){
-            continue;
-        }
-        min = pozic;
-        while (min > 0){
-            min--;
-            dolzina2 = (int)besede[min].length();
-            dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
-            if (s[0] > besede[min][0]){
-                break;
-            }
-            int pr = 0;
-            for (int i = 0; i < dolzina; i++){
-                if (s[i] != besede[min][i]){
-                    pr = 1;
-                    break;;
-                }
-            }
-            if (pr == 1){
-                continue;
-            }
-            if (t[min] < x){
-                t.erase(t.begin() + min);
-                besede.erase(besede.begin() + min);
-                pozicije.erase(pozicije.begin() + min);
-            }else{
-                break;
-            }
-        }
-        /*
-        t.insert(t.begin() + pozic, temp);
-        besede.insert(besede.begin() + pozic, s);
-        */
-    }
-    /*
-    for (int i = 0; i < (int)t.size(); i++){
-        cout << besede[i] << " " << t[i] << " " << pozicije[i] << endl;
-    }*/
-    
-    cin >> N;
-    int *izpis = new int[N];
-
-    for (int i = 0; i < N; i++){
-        cin >> s;
-        int min = 0;
-        int dolzina1 = (int)s.length();
-        int dolzina2, dolzina;
-        int rez,temp;
-        rez = 0;
-        temp = t.size() - 1;
-        dolzina2 = (int)besede[temp].length();
-        dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
-        for (int i = 0; i < dolzina; i++){
-            if (s[i] < besede[temp][i]){
-                rez = -1;
-                break;
-            }
-            else if (s[i] > besede[temp][i]){
-                rez = 1;
-                break;
-            }
-        }
-        if (rez == 0){
-            if (dolzina1 < dolzina2){
-                rez = -1;
-            }else if (dolzina1 == dolzina2){
-                rez = 0;
-            }else{
-                rez = 1;
-            }
-        }
-        if (rez == 1){
-            izpis[i] = 0;
-            continue;
-        }else{
-            rez = 0;
-            temp = 0;
-            dolzina2 = (int)besede[temp].length();
-            dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
-            for (int i = 0; i < dolzina; i++){
-                if (s[i] < besede[temp][i]){
-                    rez = -1;
-                    break;
-                }
-                else if (s[i] > besede[temp][i]){
-                    rez = 1;
-                    break;
-                }
-            }
-            if (rez == 0){
-                if (dolzina1 < dolzina2){
-                    rez = -1;
-                }else if (dolzina1 == dolzina2){
-                    rez = 0;
-                }else{
-                    rez = 1;
-                }
-            }
-            if(rez != -1){
-                int mid;
-                int max = t.size() - 1;
-                while (min < max){
-                    mid = (min + max) / 2;
-                    rez = 0;
-                    temp = mid;
-                    dolzina2 = (int)besede[temp].length();
-                    dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
-                    for (int i = 0; i < dolzina; i++){
-                        if (s[i] < besede[temp][i]){
-                            rez = -1;
-                            break;
-                        }
-                        else if (s[i] > besede[temp][i]){
-                            rez = 1;
-                            break;
-                        }
-                    }
-                    if (rez == 0){
-                        if (dolzina1 < dolzina2){
-                            rez = -1;
-                        }else if (dolzina1 == dolzina2){
-                            rez = 0;
-                        }else{
-                            rez = 1;
-                        }
-                    }
-                    if (rez == -1){
-                        max = mid;
-                    }
-                    else if (rez == 1){
-                        min = mid + 1;
-                    }else{
-                        min = mid;
-                        break;
-                    }
-                }
-            }
-        }
-        int max = 0;
-        int poz = 0;
-        while (min < (int)t.size()){
-            dolzina2 = (int)besede[min].length();
-            dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
+        int mid;
+        int min1 = min;
+        int max = N - 1;
+        while (min1 < max){
             int rez = 0;
+            mid = (min1 + max) / 2;
+            dolzina2 = (int)t[min].first.length();
+            dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
             for (int i = 0; i < dolzina; i++){
-                if (s[i] < besede[min][i]){
+                if (s[i] < t[mid].first[i]){
                     rez = -1;
+                    break;
+                }
+                else if (s[i] > t[mid].first[i]){
+                    rez = 1;
                     break;
                 }
             }
             if (rez != -1){
-                if (t[min] > max){
-                    max = t[min];
-                    poz = pozicije[min];
-                }
-            }else{
+                min1 = mid + 1;
+            }
+            else{
+                max = mid;
+            }
+        }
+        min1 = max;
+        dolzina2 = (int)t[min1].first.length();
+        dolzina = dolzina1 <= dolzina2 ? dolzina1 : dolzina2;
+        for (int i = 0; i < dolzina; i++){
+            if (s[i] < t[min1].first[i]){
+                min1--;
                 break;
+            }
+            else if (s[i] > t[min1].first[i]){
+                min1--;
+                break;
+            }
+        }
+        min1++;    
+        max = stevila[t[min].second - 1];
+        int poz = t[min].second;
+        
+        while (min < min1){
+            if (stevila[t[min].second - 1] > max){
+                max = stevila[t[min].second - 1];
+                poz = t[min].second;
             }
             min++;
         }
-        izpis[i] = poz;
+        return poz;             
     }
-    //v.izpisi();
-    for (int i = 0; i < N; i++){
-        cout << izpis[i] << endl;
+};
+
+
+
+int main(){
+    int N, x, M;
+    string s;
+    cin >> N;
+    Slovar v(N);
+    vector<int> stevila;
+    for (int i = 1; i <=N; i++){
+        cin >> s >> x;
+        v.dodaj(i, s);
+        stevila.push_back(x);
     }
+    v.sortiraj();
+    //v.izpisi(stevila);
+
+    cin >> M;
+    vector<int> rezultati;
+    for (int i = 0; i < M; i++){
+        cin >> s;
+        rezultati.push_back(v.poisci(s, stevila));
+        
+    }
+    for (int i = 0; i < (int)rezultati.size(); i++){
+        cout << rezultati[i] << endl;
+    }
+
     return 0;
 }
